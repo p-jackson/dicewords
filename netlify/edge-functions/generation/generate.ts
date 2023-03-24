3893fe5e5bf91bd74afa32405041a365cb9b6a00
Philip Jackson <p-jackson@live.com>
2023-03-25 00:31:03 +1300

    Edge function doesn't access file system
    Converted the diceware wordlist to static TypeScript data so that the
    edge function doesn't need to access the filesystem. This isn't
    currently allowed on netlify.

--- netlify/edge-functions/generation/generate.ts
+++ netlify/edge-functions/generation/generate.ts
@@ -1,12 +1,8 @@
-import { readLines } from "https://deno.land/std@0.181.0/io/read_lines.ts";
+import { wordlist } from "./diceware-wordlist.ts";
 
 const NUM_WORDS = 5;
 
 export async function generate() {
-  const file = await Deno.open(
-    "netlify/edge-functions/generation/diceware.wordlist.asc.txt"
-  );
-
   const phrase = Array.from({ length: NUM_WORDS }).map(() => {
     const randoms = new Uint32Array(5);
     crypto.getRandomValues(randoms);
@@ -15,17 +11,5 @@ export async function generate() {
       .join("");
   });
 
-  let found = 0;
-  for await (const line of readLines(file)) {
-    for (let i = 0; i < phrase.length; i++) {
-      const word = phrase[i];
-      if (line.startsWith(word)) {
-        phrase[i] = line.split("\t")[1];
-        found++;
-      }
-    }
-    if (found >= NUM_WORDS) break;
-  }
-
-  return phrase;
+  return phrase.map((word) => wordlist[word]);
 }
